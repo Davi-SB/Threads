27af681f1695d18ff3e8680164f068738816950d
Davi-SB <davibrilhante0102@gmail.com>
2024-05-09 19:56:49 -0300

    aula2 estudos1

--- Slides - Exclusao Mutua/2.uso_mutex.c
+++ Slides - Exclusao Mutua/2.uso_mutex.c
@@ -2,27 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long contador = 0; 
-pthread_mutex_t mymutex = PTHREAD_MUTEX_INITIALIZER;
-void *inc(void *threadid){
-  int i = 0; for(; i < 9000000; i++) { 
-    pthread_mutex_lock(&mymutex); 
-    contador++; 
-    pthread_mutex_unlock(&mymutex); }
+long contador = 0;
+pthread_mutex_t myMutex = PTHREAD_MUTEX_INITIALIZER; // geralmente eh criado um mutex para cada regiao critica
+
+void *inc(void *threadid) {
+    for (int i = 0; i < 9000000; i++) {
+        pthread_mutex_lock(&myMutex); // se T1 tentar travar o mutex e ele ja estiver travado, T1 fica travada ate que o mutex esteja desbloqueado
+        contador++;
+        pthread_mutex_unlock(&myMutex);
+    }
 }
-void *dec(void *threadid){   
-  int i = 0;
-  for(; i < 9000000; i++) { 
-    pthread_mutex_lock(&mymutex); 
-    contador--; 
-    pthread_mutex_unlock(&mymutex); }
+
+void *dec(void *threadid) {
+    for (int i = 0; i < 9000000; i++) {
+        pthread_mutex_lock(&myMutex); // lock eh bloqueante, mas trylock, nao. Espera ocupada, se tivver travado continua checando. retorna 0 quando consegue bloquear. 
+        contador--;
+        pthread_mutex_unlock(&myMutex);
+    }
 }
-int main (int argc, char *argv[]){   
-  pthread_t thread1, thread2;   
-  pthread_create(&thread1, NULL, inc, NULL); 
-  pthread_create(&thread2, NULL, dec, NULL); 
-  pthread_join(thread1, NULL);
-  pthread_join(thread2, NULL); 
-  printf("Valor final do contador: %ld\n", contador);
-  pthread_exit(NULL);
+
+int main(int argc, char *argv[]) {
+    pthread_t thread1, thread2;
+    pthread_create(&thread1, NULL, inc, NULL);
+    pthread_create(&thread2, NULL, dec, NULL);
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+    printf("Valor final do contador: %ld\n", contador);
+    pthread_exit(NULL);
 }
--- Slides - Exclusao Mutua/3.prod_cons.c
+++ Slides - Exclusao Mutua/3.prod_cons.c
@@ -1,75 +1,83 @@
 #include <stdio.h>
-#include <pthread.h> 
+#include <pthread.h>
 #include <stdlib.h>
 
 #define BUFFER_SIZE 10
-#define NUM_ITEMS 200
+#define NUM_ITEMS 200 // num de itens a serem inseridos
 
-int buff[BUFFER_SIZE];  /* buffer size = 10; */
-int items = 0; // number of items in the buffer.
+int buff[BUFFER_SIZE];
+int items = 0; // num de itens no buffer
 int first = 0;
-int last = 0; 
+int last  = 0;
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t empty = PTHREAD_COND_INITIALIZER;
-pthread_cond_t fill = PTHREAD_COND_INITIALIZER;
- 
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;// Enquanto o mutex eh usado para garantir a exclusao mutua, as variaveis de condicao sao usadas para notificar outras threads quando uma condicao importante eh alcancada.
+pthread_cond_t empty  = PTHREAD_COND_INITIALIZER; // VARIAVEIS DE CONDICAO PRECISAM SER SEMPRE USADAS COM MUTEX!!!!
+pthread_cond_t fill   = PTHREAD_COND_INITIALIZER; // Elas permitem que threads esperem por determinadas condicoes antes de continuar sua execucao
+
 void *producer();
 void *consumer();
+void put(int e);
+int  get();
 
 int main() {
-  pthread_t consumer_thread; 
-  pthread_t producer_thread; 
-  pthread_create(&consumer_thread,NULL,consumer,NULL);
-  pthread_create(&producer_thread,NULL,producer,NULL);
-  pthread_join(producer_thread,NULL);  
-  pthread_join(consumer_thread,NULL);
+    pthread_t consumer_thread;
+    pthread_t producer_thread;
+    pthread_create(&consumer_thread, NULL, consumer, NULL);
+    pthread_create(&producer_thread, NULL, producer, NULL);
+    
+    pthread_join(producer_thread, NULL);
+    pthread_join(consumer_thread, NULL);
 }
 
-void put(int i){
-  pthread_mutex_lock(&mutex);
-  while(items == BUFFER_SIZE) {
-    pthread_cond_wait(&empty, &mutex);
-  }
-  buff[last] = i;
-  printf("pos %d: ", last);
-  items++; last++;
-  if(last==BUFFER_SIZE) { last = 0; } 
-  if(items == 1) { 
-    pthread_cond_signal(&fill); }
-  pthread_mutex_unlock(&mutex); 
+/*
+pthread_cond_wait(): Bloqueia a thread atual que que seja sinalizado que a condicao associada foi alcancada
+pthread_cond_signal(): Sinaliza que a condicao foi alcancada, despertando uma thread que estra esperando por essa condicao.
+pthread_cond_broadcast(): Sinaliza que a condicao foi alcancada, despertando todas as threads que estao esperando por essa condicao. 
+*/
+
+void put(int e) {
+    pthread_mutex_lock(&mutex); // Esta linha trava o mutex para garantir que apenas um thread por vez possa executar a secao critica do codigo, ou seja, a parte do codigo que pode acessar e modificar variaveis compartilhadas, como items, first, last e buff. tem que estar antes de qualquer coisa nesse caso pois tudo acessa as variaveis compartilhadas
+
+    while(items == BUFFER_SIZE) pthread_cond_wait(&empty, &mutex); // while eh boa pratica para evitar sinais falsos (spurious wakeup). em caso de buffer cheio, o wait vai ser chamado. Ao chamar wait, a thread eh bloqueada e o mutex (segundo argumento) eh automaticamente liberado. Assim outras threads podem usar o mutex e acessar a sessao critica enquanto essa thread continua bloquada. o primeiro argumento eh a condicao de desbloqueio da thread. Quando o singal manda um sinal pra variavel do primeiro argumento, essa thread eh liberada (desperada) e tenta automaticamente pegar o mutex "de volta", travar, e assim entao poder continuar a sua execucao
+    buff[last] = e; // insere o elemento
+    printf("pos %d: ", last); // informa o a posicao em que o ultimo elemento inserido se encontra
+    items++; last++;
+
+    last %= BUFFER_SIZE; // if (last == BUFFER_SIZE) last = 0; // modular (array circular)
+    if (items == 1) pthread_cond_signal(&fill); // se items eh 1, ele estava vazio antes. Logo, o consumidor pode estar bloqueado (wait). Entao eh enviado um sinal para a variavel em que ele espera o sinal em caso de bloqueio, o fill, para que seja desbloqueado
+
+    pthread_mutex_unlock(&mutex);
 }
+
 void *producer() {
-  int i = 0;
-  printf("Produtor\n");
-  for(i=0;i<NUM_ITEMS; i++) {
-    put(i);
-    printf("Produzi %d \n",i);
-  }
-  pthread_exit(NULL);
+    printf("Produtor\n");
+    for (int i = 0; i < NUM_ITEMS; i++) { // produtor produz NUM_ITEMS itens e depois termina
+        put(i);
+        printf("Produzi %d \n", i);
+    }
+    pthread_exit(NULL);
 }
 
-int get(){
-  int result;
-  pthread_mutex_lock(&mutex);
- while(items == 0){
-    pthread_cond_wait(&fill, &mutex);
-  }
-  result = buff[first];
-  printf("pos %d: ", first);
-  items--; first++;
-  if(first==BUFFER_SIZE) { first = 0; }
-  if(items == BUFFER_SIZE - 1){
-    pthread_cond_signal(&empty);}
-  pthread_mutex_unlock(&mutex);
-  return result;
+int get() {
+    int result;
+    pthread_mutex_lock(&mutex);
+
+    while (items == 0) pthread_cond_wait(&fill, &mutex); // espera no caso de buff vazio ate que receba um sinal para encher (fill)
+    result = buff[first]; // retorno eh o elemento que removeu, o primeiro
+    printf("pos %d: ", first);
+    items--; first++; // lazy deletion
+    first %= BUFFER_SIZE; // if (first == BUFFER_SIZE) first = 0;
+    if (items == BUFFER_SIZE - 1) pthread_cond_signal(&empty); // se estava cheio, o produtor pode estar bloqueado. Se for o caso, tem que acordar
+    
+    pthread_mutex_unlock(&mutex);
+    return result;
 }
+
 void *consumer() {
-  int i,v;
-  printf("Consumidor\n");
-  for (i=0;i<NUM_ITEMS;i++) {
-    v = get();
-    printf("Consumi %d  \n",v);
-  }
-  pthread_exit(NULL);
+    printf("Consumidor\n");
+    for (int i = 0; i < NUM_ITEMS; i++) { // consome NUM_ITEMS itens e depois termina
+        int v = get();
+        printf("Consumi %d\n", v);
+    }
+    pthread_exit(NULL);
 }
