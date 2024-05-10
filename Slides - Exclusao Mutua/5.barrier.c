4e3fe77c9136628df44146bed227ddebd05945e2
Davi-SB <davibrilhante0102@gmail.com>
2024-05-09 23:23:05 -0300

    slides finalizados

--- Slides - Exclusao Mutua/5.barrier.c
+++ Slides - Exclusao Mutua/5.barrier.c
@@ -7,40 +7,33 @@
 #define THREADS 4
 #define ITERACOES 5
 
-pthread_barrier_t barrier;
-
+pthread_barrier_t barrier; // nao pode ser inicializada estaticamente (aqui)
 void *codigo_thread(void *threadid);
 
 int main() {
-
+    pthread_barrier_init(&barrier, NULL, THREADS); // inicializacao da barreira dinamicamente (destroy necessario depois). Terceiro argumento eh o numero de threads que a barreira vai alinhar. A barreira vai segurando threads e quando alcança essa quantidade, libera todas (sincronia)
     pthread_t threads[THREADS];
     int *ids[THREADS];
-
-    pthread_barrier_init(&barrier, NULL, THREADS);
 
     for (int i = 0; i < THREADS; i++) {
         ids[i] = (int *)malloc(sizeof(int));
         *ids[i] = i;
-        pthread_create(&threads[i], NULL, codigo_thread, (void *)ids[i]);
+        pthread_create(&threads[i], NULL, codigo_thread, (void *) ids[i]);
     }
 
-    for (int i = 0; i < THREADS; i++) {
-        pthread_join(threads[i], NULL);
-    }
-
-    pthread_barrier_destroy(&barrier);
+    for (int i = 0; i < THREADS; i++) pthread_join(threads[i], NULL);
+    pthread_barrier_destroy(&barrier); // libera a alocacao da barreira
     pthread_exit(NULL);
-    return 0;
 }
 
 void *codigo_thread(void *threadid) {
-    printf("Thread %d iniciou \n", *((int *)threadid));
+    printf("Thread %d iniciou \n", *((int *)threadid) );
 
-    pthread_barrier_wait(&barrier);
+    // pthread_barrier_wait(&barrier);
 
     for (int i = 0; i < ITERACOES; i++) {
         printf("Thread %d iteracao %d \n", *((int *)threadid), i);
-        pthread_barrier_wait(&barrier);
+        pthread_barrier_wait(&barrier); // coloca a thread que chamou o barrier_wait na barreira, fica bloqueada lá
     }
 
     printf("Thread %d terminou \n", *((int *)threadid));
